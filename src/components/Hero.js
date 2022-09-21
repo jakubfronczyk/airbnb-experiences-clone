fdffdef434961a6dc3afd0e7088eb88052129a9b
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-21 17:30:04 +0200

    Added Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,8 @@ export default function Hero(){
         <section className="hero-section">
             <img src={grid} alt="Aribnb Experiences" className="hero-photo" />
             <div className="hero-info">    
-                <h1 className="hero-title hero-info">Online Experiences</h1>
-                <p className="hero-description hero-info">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+                <h1 className="hero-title">Online Experiences</h1>
+                <p className="hero-description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
             </div>    
         </section>
     )
