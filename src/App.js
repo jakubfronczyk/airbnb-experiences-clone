1aae1d4af3ebdb3f465fc9edde2914594e37b332
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-21 17:29:27 +0200

    Added Card using props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+// import JokeProps from './components/JokeProps.js';
 import Navbar from './components/Navbar.js';
-// import Hero from './components/Hero.js';
+import Hero from './components/Hero.js';
 import Card from './components/Card.js';
+import experience1 from './images/experience-1.png';
+// import Props from './components/Props.js';
 
 function App() {
   return (
     <div className="">
       <Navbar />
-      {/* <Hero /> */}
-      <Card />
+      <Hero />
+      <Card
+      img={experience1}
+      status="SOLD OUT"
+      rating="5.0"
+      reviewCount={6}
+      country="USA"
+      title="Life Lessons with Katie Zaferes"
+      price={136}
+      />
     </div>
   );
 }
