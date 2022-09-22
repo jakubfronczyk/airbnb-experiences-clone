303f3ba44e5f90af7c063ac8631e42c53c49391f
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-22 19:32:57 +0200

    Added Card info using map

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,13 +6,13 @@ export default function Card(props){
     return(
         <section className="card-section">
             <div className="card">
-                <img src={props.img} alt="Arbnb Expirience" className="card-image"/>
-                <p className="card-status">{props.status}</p>
+                <img src={props.coverImg} alt="Arbnb Expirience" className="card-image"/>
+                {props.status && <p className="card-status">{props.status}</p>}
                 <div className="card-opinion">
                     <img src={star} alt="Star" className="card-star"/>
                     <span>{props.rating}</span>
                     <span className="grey">({props.reviewCount}) • </span>
-                    <span className="grey">{props.country}</span>
+                    <span className="grey">{props.location}</span>
                 </div>
                 <p>{props.title}</p>
                 <p><span className="bold">From ${props.price}</span> / person</p>
