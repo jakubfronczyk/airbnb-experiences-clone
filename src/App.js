0f9d58ad9d0dbac08bde8b554495826625ce698b
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-22 19:33:26 +0200

    Added Card info using map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-// import JokeProps from './components/JokeProps.js';
 import Navbar from './components/Navbar.js';
 import Hero from './components/Hero.js';
 import Card from './components/Card.js';
-import experience1 from './images/experience-1.png';
+import data from './data.js'
+// import JokeProps from './components/JokeProps.js';
 // import Props from './components/Props.js';
 
 function App() {
+
+  const dataMap = data.map(data => {
+    return (
+      <Card
+      id= {data.id}
+      title= {data.title}
+      description= {data.description}
+      price= {data.price}
+      coverImg= {data.coverImg}
+      status= {data.status}
+      rating= {data.rating}
+      reviewCount= {data.reviewCount}
+      location= {data.location}
+      openSpots= {data.openSpots}
+      />
+    )
+  })
+
   return (
     <div className="">
       <Navbar />
       <Hero />
-      <Card
-      img={experience1}
-      status="SOLD OUT"
-      rating="5.0"
-      reviewCount={6}
-      country="USA"
-      title="Life Lessons with Katie Zaferes"
-      price={136}
-      />
+      {dataMap}
     </div>
   );
 }
